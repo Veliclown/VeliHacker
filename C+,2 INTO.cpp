a1c04dea91f00620145c7e25561aefa78a1ce02e
Veliclown <117837346+Veliclown@users.noreply.github.com>
2022-11-09 16:35:42 +0200

    Сreating code with two ints.С++
    Сreating code with two ints.С++

---
+++ C+,2 INTO.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream> 
+#include <iomanip> 
+#include "windows.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
+using namespace std;
+int f(double x, double e, double z)
+{
+	int c;
+	c = (x * (tan(z)+pow(e,(-x-3)) + pow(cos(180 * M_PI / 180), 2)));
+	return c;
+}
+int main()
+{
+	int x, e , z;
+	cout << "Enter x, e , z" << endl;
+	cin >> x >> e >> z;
+	cout << f(x, e , z) << endl;
+}
